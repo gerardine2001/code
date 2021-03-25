f9d85ec49949674a9cfa3ac275442973cd895ac6
gerardine <gerardinekaba@gmail.com>
2021-03-25 15:53:27 +0300

    week6

---
+++ week6.js
@@ -0,0 +1,12 @@
+var txt=" ilove akirachix";
+console.log(txt.length);
+var x="we are so \"proud \" of Rwanda";
+console.log(x);
+ var a="i love \b akrachix";
+console.log(a);
+var a="i love \n akirachix";
+console.log(a);
+
+var q="I am \t passionate \n in technology";
+console.log(q);
+
